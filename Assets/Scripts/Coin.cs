2aad10df4b5a2a16d14402b3c9e8389c6007ad70
filebin <alex.mousse9@gmail.com>
2023-08-25 23:49:49 +0300

    coin logic

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace MultiplayerTask {
+    public class Coin : MonoBehaviour {
+        public bool IsCollectable { get; private set; }
+
+        private void Start() {
+            IsCollectable = true;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            var player = other.GetComponent<Player>();
+            if (player != null) {
+                Collect(player);
+            }
+        }
+
+        private void Collect(Player player) {
+            if (IsCollectable) {
+                IsCollectable = false;
+                player.Coins++;
+                DestroyImmediate(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace MultiplayerTask {
+    public class Player : MonoBehaviour {
+        public int Coins { get; set; }
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+    }
+}
